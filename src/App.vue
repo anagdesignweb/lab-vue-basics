aee629e32be5a3f1dee118f670c09f54d1516995
Ana Garcia <anagdesignweb@gmail.com>
2022-10-13 13:18:22 +0200

    hasta iteracion 4

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <Navbar/>
+  <Section/>
+  <Footer/>
+
+</template>
+
+
+<script setup>
+  import Navbar from './components/Navbar.vue';
+  import Section from './components/Section.vue';
+  import Footer from './components/Footer.vue';
+</script>
+
+
+<style scoped>
+</style>
